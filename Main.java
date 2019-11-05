a1ad8183abc83a2169f6bb6f591f785eababd7e7
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2019-11-05 20:41:43 +0000

    fdfg

--- Main.java
+++ Main.java
@@ -1,43 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
-class gui {
-    public static void main(String args[]) {
+import java.awt.event.*;
 
-        //Creating the Frame
-        JFrame frame = new JFrame("Chat Frame");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-
-        //Creating the MenuBar and adding components
-        JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("FILE");
-        JMenu m2 = new JMenu("Help");
-        mb.add(m1);
-        mb.add(m2);
-        JMenuItem m11 = new JMenuItem("Open");
-        JMenuItem m22 = new JMenuItem("Save as");
-        m1.add(m11);
-        m1.add(m22);
-
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
-        JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JButton send = new JButton("Send");
-        JButton reset = new JButton("Reset");
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
-        panel.add(send);
-        panel.add(reset);
-
-        // Text Area at the Center
-        JTextArea ta = new JTextArea();
-
-        //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.getContentPane().add(BorderLayout.NORTH, mb);
-        frame.getContentPane().add(BorderLayout.CENTER, ta);
+class GuiTesting{
+    public static void main (String[] args){
+        JFrame frame = new JFrame("test");
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        JButton button = new JButton("Click here");
+        panel.add(button);
+        button.addActionListener(new Action());
+    }
+    static class Action implements ActionListener{
+        public void actionPerformed (ActionEvent e){
+            JFrame frame2 = new JFrame("clicked");
+            frame2.setVisible(true);
+            frame2.setSize(300,300);
+            JPanel panel2 = new JPanel();
+            JLabel label = new JLabel("You clicked me!");
+            frame2.add(panel2);
+            panel2.add(label);
+        }
     }
 }
