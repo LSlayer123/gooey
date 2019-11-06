b23fcd8772f2d00bace26e85342a510ce390d3af
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2019-11-06 18:57:13 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,10 +8,34 @@ class GuiTesting{
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,400);
-        JPanel panel = new JPanel();
-        frame.add(panel);
+        JPanel panel = new JPanel(new GridBagLayout());
+        frame.getContentPane().add(panel, BorderLayout.NORTH);
+        GridBagConstraints constraints = new GridBagConstraints();
+        JLabel label1 = new JLabel("Test1");
+        JLabel label2 = new JLabel("Test2");
+        JLabel label3 = new JLabel("Test3");
+        JLabel label4 = new JLabel("Test4");
+        JLabel label5 = new JLabel("Test5");
         JButton button = new JButton("Click here");
-        panel.add(button);
+        constraints.insets = new Insets(5,5,5,5);
+        constraints.gridx = 5;
+        constraints.gridy = 5;
+        panel.add(label1, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        panel.add(label2, constraints);
+        constraints.gridx = 2;
+        constraints.gridy = 2;
+        panel.add(label3, constraints);
+        constraints.gridx = 3;
+        constraints.gridy = 3;
+        panel.add(label4, constraints);
+        constraints.gridx = 4;
+        constraints.gridy = 4;
+        panel.add(label5, constraints);
+        constraints.gridx = 8;
+        constraints.gridy = 5;
+        panel.add(button, constraints);
         button.addActionListener(new Action());
     }
     static class Action implements ActionListener{
